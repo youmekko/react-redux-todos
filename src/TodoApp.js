662d4b8ca33bc98cfb79d4b308de800979b150b8
youmekko <youmekko.o@gmail.com>
2020-08-05 08:55:50 +0900

    feat : TodoApp 프로젝트 생성 및 스캐폴딩

---
+++ src/TodoApp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+function TodoApp() {
+  return (
+    <div>
+       TodoApp
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+import TodoApp from './TodoApp';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <TodoApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
